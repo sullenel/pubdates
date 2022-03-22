1580369ae74801adfc644c2d4abb0c7f7e00bf49
sullenel <sullenel@users.noreply.github.com>
2022-03-22 17:01:58 +0400

    fix: not all changelogs are sorted in descending order

--- lib/features/changelog/services/changelog_parser.dart
+++ lib/features/changelog/services/changelog_parser.dart
@@ -1,6 +1,37 @@
 import 'package:html/dom.dart' as dom;
 import 'package:html/parser.dart' as html;
+import 'package:pub_semver/pub_semver.dart';
 import 'package:pubdates/features/changelog/models/changelog_content.dart';
+
+extension on List<ChangeLogContent> {
+  // In some package changelogs, versions are specified in ascending order.
+  List<ChangeLogContent> get sortedInDescendingOrder {
+    if (_isInDescendingOrder) {
+      return this;
+    }
+
+    return this..sort(_compareVersionsInDescendingOrder);
+  }
+
+  bool get _isInDescendingOrder {
+    // It is more than enough.
+    if (length > 1) {
+      final current = Version.parse(this[0].version);
+      final next = Version.parse(this[1].version);
+      return current > next;
+    }
+
+    return true;
+  }
+
+  // As if a mad Swift developer bit me.
+  int _compareVersionsInDescendingOrder(
+    ChangeLogContent a,
+    ChangeLogContent b,
+  ) {
+    return Version.parse(b.version).compareTo(Version.parse(a.version));
+  }
+}
 
 class ChangeLogParser {
   const ChangeLogParser();
