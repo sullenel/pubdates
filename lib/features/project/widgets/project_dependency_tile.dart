8eb38a1a9146b0c4c5ba2ea622aad2d5ea8c7eb2
sullenel <sullenel@users.noreply.github.com>
2022-04-10 02:13:38 +0400

    refactor: extract a widget for list tile

--- lib/features/project/widgets/project_dependency_tile.dart
+++ lib/features/project/widgets/project_dependency_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pubdates/common/constants/dimensions.dart';
 import 'package:pubdates/common/constants/icons.dart';
+import 'package:pubdates/common/widgets/custom_list_tile.dart';
 import 'package:pubdates/common/widgets/space.dart';
 import 'package:pubdates/features/project/models/package.dart';
 
@@ -26,63 +27,61 @@ class ProjectDependencyTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final colors = theme.colorScheme;
-    final textTheme = theme.textTheme;
-
     return Padding(
       padding: const EdgeInsets.symmetric(
         vertical: AppInsets.md,
         horizontal: AppInsets.lg,
       ),
-      child: InkWell(
-        onTap: onPressed,
-        borderRadius: AppBorders.button,
-        child: Ink(
-          decoration: BoxDecoration(
-            borderRadius: AppBorders.button,
-            color: Colors.black.withOpacity(0.2),
-          ),
-          padding: const EdgeInsets.symmetric(
-            vertical: AppInsets.md,
-            horizontal: AppInsets.lg,
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        package.name,
-                        style: textTheme.titleMedium?.copyWith(
-                          color: colors.onSecondary,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: AppInsets.md),
-                    Flexible(
-                      child: Text(
-                        package.upgradePath,
-                        style: textTheme.bodySmall?.copyWith(
-                          color: colors.onSecondary.withOpacity(0.5),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              if (package.canBeUpgraded) ...const [
-                HSpace(AppInsets.md),
-                Icon(AppIcons.circle, color: Colors.orange, size: 10),
-              ],
-            ],
+      child: CustomListTile(
+        onPressed: onPressed,
+        // FIXME: the color must come from the theme
+        backgroundColor: Colors.black.withOpacity(0.2),
+        trailing: package.canBeUpgraded
+            ? const Icon(AppIcons.circle, color: Colors.orange, size: 10)
+            : null,
+        child: _PackageInfoSection(package: package),
+      ),
+    );
+  }
+}
+
+class _PackageInfoSection extends StatelessWidget {
+  const _PackageInfoSection({
+    Key? key,
+    required this.package,
+  }) : super(key: key);
+
+  final Package package;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colors = theme.colorScheme;
+    final textTheme = theme.textTheme;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Flexible(
+          child: Text(
+            package.name,
+            style: textTheme.titleMedium?.copyWith(
+              color: colors.onSecondary,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
-      ),
+        const VSpace(AppInsets.md),
+        Flexible(
+          child: Text(
+            package.upgradePath,
+            style: textTheme.bodySmall?.copyWith(
+              color: colors.onSecondary.withOpacity(0.5),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
