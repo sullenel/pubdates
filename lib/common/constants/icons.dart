029e33621ba18175366431b5d57dce7161e11f52
sullenel <sullenel@users.noreply.github.com>
2022-07-17 02:10:42 +0400

    refactor: simplify home page logo section

--- lib/common/constants/icons.dart
+++ lib/common/constants/icons.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 mixin AppIcons {
   static const openLink = Icons.open_in_new;
   static const circle = Icons.circle;
-  static const upgrade = Icons.upgrade;
   static const arrowRight = Icons.chevron_right;
   static const remove = Icons.highlight_remove;
   static const sort = Icons.sort;
