0ce7953e1e6ab7745dc9656b8d01aabf1d305725
sullenel <sullenel@users.noreply.github.com>
2022-07-17 13:07:24 +0400

    refactor: use CupertinoActivityIndicator on macOS

--- lib/common/widgets/loading_indicator.dart
+++ lib/common/widgets/loading_indicator.dart
@@ -1,15 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pubdates/common/utils/flutter_utils.dart';
+import 'package:pubdates/common/utils/platform_utils.dart';
+import 'package:pubdates/common/widgets/space.dart';
+import 'package:pubdates/localization/app_localizations.dart';
 
 class LoadingIndicator extends StatelessWidget {
-  const LoadingIndicator({Key? key}) : super(key: key);
+  const LoadingIndicator({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: SizedBox.square(
-        dimension: 24,
-        child: CircularProgressIndicator(strokeWidth: 4),
-      ),
+    return Center(
+      child: isMacOS
+          ? const _AppleLoadingIndicator()
+          : const SizedBox.square(
+              dimension: 24,
+              child: CircularProgressIndicator(strokeWidth: 3),
+            ),
+    );
+  }
+}
+
+class _AppleLoadingIndicator extends StatelessWidget {
+  const _AppleLoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = context.textTheme;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const CupertinoActivityIndicator(),
+        const VSpace.small(),
+        Text(
+          AppLocalizations.of(context).loading,
+          style: textTheme.caption,
+        ),
+      ],
     );
   }
 }
